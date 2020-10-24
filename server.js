b22fe4416338574a694b08d308f2048dc027f442
Martin Passada <mpassada@openpass.com.ar>
2020-10-24 12:51:27 -0300

    fixes

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function authenticateToken(req, res, next) {
     })
 }
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1800' })
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1200s' })
 }
 
 /*setInterval(async () => {
